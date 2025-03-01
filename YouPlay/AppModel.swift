61c98a540ea7388ba01bb8e48b8d873d6ed7e985
Simon Kobler <simon@kobler.me>
2025-03-01 17:47:58 +0700

    Initial Commit

---
+++ YouPlay/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  YouPlay
+//
+//  Created by Simon Kobler on 01.03.25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ YouPlay/YouPlayApp.swift
@@ -0,0 +1,33 @@
+//
+//  YouPlayApp.swift
+//  YouPlay
+//
+//  Created by Simon Kobler on 01.03.25.
+//
+
+import SwiftUI
+
+@main
+struct YouPlayApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
